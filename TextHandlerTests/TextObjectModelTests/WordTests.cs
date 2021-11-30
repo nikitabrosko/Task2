9b73a0a7f587dbd3f3bacbc3a1df658b71e082c2
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-30 16:48:31 +0300

    Create a WordTests class

---
+++ TextHandlerTests/TextObjectModelTests/WordTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel;
+using TextHandler.TextObjectModel.Characters.Letters;
+
+namespace TextHandlerTests.TextObjectModelTests
+{
+    [TestClass]
+    public class WordTests
+    {
+        [TestMethod]
+        public void TestWordClassCreating()
+        {
+            var letters = new List<Letter>
+            {
+                new Letter('w'),
+                new Letter('o'),
+                new Letter('r'),
+                new Letter('d')
+            };
+
+            var wordObject = new Word(letters);
+
+            Assert.IsTrue(letters.SequenceEqual(wordObject.Value.ToList()));
+        }
+    }
+}
