3e3f2090235f37bc46d1f24dfb5b5b7ff9160759
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-30 16:14:57 +0300

    Change implementation of Word class

--- TextHandler/TextWorks/Word.cs
+++ TextHandler/TextWorks/Word.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextHandler.TextWorks.Characters.Letters;
 
 namespace TextHandler.TextWorks
 {
-    public class Word
+    public class Word : ISentenceable
     {
+        private readonly IList<Letter> _word;
+
+        public IEnumerable<Letter> Value => new ReadOnlyCollection<Letter>(_word);
+
+        public Word(IEnumerable<Letter> letters)
+        {
+            _word = letters.ToList();
+        }
     }
 }
