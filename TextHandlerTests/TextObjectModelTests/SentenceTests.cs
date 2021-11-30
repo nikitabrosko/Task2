7935675b5cda432499b41c357db15af58cc6a7da
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-30 16:59:37 +0300

    Create a test class SentenceTests

---
+++ TextHandlerTests/TextObjectModelTests/SentenceTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel;
+using TextHandler.TextObjectModel.Characters.Letters;
+using TextHandler.TextObjectModel.Characters.Punctuation;
+
+namespace TextHandlerTests.TextObjectModelTests
+{
+    [TestClass]
+    public class SentenceTests
+    {
+        [TestMethod]
+        public void TestSentenceClassCreating()
+        {
+            var sentenceElements = new List<ISentenceable>
+            {
+                new Word(new Letter[]
+                {
+                    new Letter('s'), 
+                    new Letter('e'), 
+                    new Letter('n'), 
+                    new Letter('t'), 
+                    new Letter('e'), 
+                    new Letter('n'), 
+                    new Letter('c'), 
+                    new Letter('e')
+                }),
+                new Word(new Letter[]
+                {
+                    new Letter('h'),
+                    new Letter('a'),
+                    new Letter('v'),
+                    new Letter('e')
+                }),
+                new Word(new Letter[]
+                {
+                    new Letter('f'),
+                    new Letter('o'),
+                    new Letter('u'),
+                    new Letter('r')
+                }),
+                new Word(new Letter[]
+                {
+                    new Letter('w'),
+                    new Letter('o'),
+                    new Letter('r'),
+                    new Letter('d'),
+                    new Letter('s')
+                }),
+                new PunctuationMark('!')
+            };
+
+            var sentenceObject = new Sentence(sentenceElements);
+
+            Assert.IsTrue(sentenceElements.SequenceEqual(sentenceObject.Value.ToList()));
+        }
+    }
+}
