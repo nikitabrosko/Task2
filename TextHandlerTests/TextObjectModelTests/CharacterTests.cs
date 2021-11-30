19e0a12fbe7e952c5cef5bb19867cc8341077f28
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-30 16:43:27 +0300

    Create a test class CharacterTests

---
+++ TextHandlerTests/TextObjectModelTests/CharacterTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel.Characters.Letters;
+using TextHandler.TextObjectModel.Characters.Punctuation;
+
+namespace TextHandlerTests.TextObjectModelTests
+{
+    [TestClass]
+    public class CharacterTests
+    {
+        [TestMethod]
+        [DataRow('a')]
+        [DataRow('b')]
+        [DataRow('c')]
+        public void TestLetterClassCreating(char letter)
+        {
+            var letterObject = new Letter(letter);
+
+            Assert.AreEqual(letter, letterObject.Value);
+        }
+
+        [TestMethod]
+        [DataRow('.')]
+        [DataRow(',')]
+        [DataRow('?')]
+        public void TestPunctuationMarkClassCreating(char punctuationMark)
+        {
+            var punctuationMarkObject = new PunctuationMark(punctuationMark);
+
+            Assert.AreEqual(punctuationMark, punctuationMarkObject.Value);
+        }
+
+        [TestMethod]
+        public void TestPunctuationSymbolClassCreating()
+        {
+            var punctuationMarks = new List<PunctuationMark>
+            {
+                new PunctuationMark('.'), 
+                new PunctuationMark(','), 
+                new PunctuationMark('?')
+            };
+
+            var punctuationSymbolObject = new PunctuationSymbol(punctuationMarks);
+
+            Assert.IsTrue(punctuationMarks.SequenceEqual(punctuationSymbolObject.Value.ToList()));
+        }
+    }
+}
