8a68b722c287da0127de5d55e7f493f3eb9427ac
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-30 16:15:22 +0300

    Change implementation of Text class

--- TextHandler/TextWorks/Text.cs
+++ TextHandler/TextWorks/Text.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,13 @@ namespace TextHandler.TextWorks
 {
     public class Text
     {
+        private readonly IList<Sentence> _text = new List<Sentence>();
+
+        public IEnumerable<Sentence> Value => new ReadOnlyCollection<Sentence>(_text);
+
+        public void Append(Sentence sentence)
+        {
+            _text.Add(sentence);
+        }
     }
 }
