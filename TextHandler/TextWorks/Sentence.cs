38b988509ef68bbcafceec866b092876c12a8af5
Nik.Brosko <Nikita.brosko@mail.ru>
2021-11-30 16:15:13 +0300

    Change implementation of Sentence class

--- TextHandler/TextWorks/Sentence.cs
+++ TextHandler/TextWorks/Sentence.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextHandler.TextWorks.Characters.Punctuation;
 
 namespace TextHandler.TextWorks
 {
     public class Sentence
     {
+        private readonly IList<ISentenceable> _sentence = new List<ISentenceable>();
+
+        public IEnumerable<ISentenceable> Value => new ReadOnlyCollection<ISentenceable>(_sentence);
+
+        public Sentence(IEnumerable<ISentenceable> sentenceElements)
+        {
+            _sentence = sentenceElements.ToList();
+        }
     }
 }
