9e913103f619902a38e276b0479fb7c6a4998157
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-04 17:34:58 +0300

    Add a Verify method to class Letter

--- TextHandler/TextObjectModel/Characters/Letters/Letter.cs
+++ TextHandler/TextObjectModel/Characters/Letters/Letter.cs
@@ -10,13 +10,36 @@ namespace TextHandler.TextObjectModel.Characters.Letters
 
             try
             {
-                Verifier.Verify(this);
+                Verify(this);
             }
             catch
             {
                 Value = default;
 
                 throw;
+            }
+        }
+
+        public static void Verify(Letter letter)
+        {
+            if (letter is null)
+            {
+                throw new ArgumentNullException(nameof(letter));
+            }
+
+            if (char.IsDigit(letter.Value))
+            {
+                throw new ArgumentException("letter can not be a digit", nameof(letter));
+            }
+
+            if (char.IsWhiteSpace(letter.Value))
+            {
+                throw new ArgumentException("letter can not be a whitespace", nameof(letter));
+            }
+
+            if (char.IsPunctuation(letter.Value))
+            {
+                throw new ArgumentException("letter can not be a punctuation", nameof(letter));
             }
         }
     }
