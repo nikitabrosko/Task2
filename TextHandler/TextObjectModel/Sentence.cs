2a8811e316810d1f3f86f2017d33f48a74c2251e
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-04 17:35:39 +0300

    Add a Verify method to class Sentence

--- TextHandler/TextObjectModel/Sentence.cs
+++ TextHandler/TextObjectModel/Sentence.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using TextHandler.TextObjectModel.Characters.Punctuation;
 
 namespace TextHandler.TextObjectModel
 {
@@ -16,13 +18,46 @@ namespace TextHandler.TextObjectModel
 
             try
             {
-                Verifier.Verify(this);
+                Verify(this);
             }
             catch
             {
                 _sentence = default;
 
                 throw;
+            }
+        }
+
+        public static void Verify(Sentence sentence)
+        {
+            if (sentence is null)
+            {
+                throw new ArgumentNullException(nameof(sentence));
+            }
+
+            if (sentence.Value is null)
+            {
+                throw new ArgumentNullException(nameof(sentence));
+            }
+
+            var firstSentenceElement = sentence.Value.ToList().First();
+
+            switch (firstSentenceElement)
+            {
+                case PunctuationMark or PunctuationSymbol:
+                    throw new ArgumentException("first element of sentence can not be a punctuation mark or symbol");
+                case Word word when char.IsLower(word.Value.ToList()[0].Value):
+                    throw new ArgumentException("first element of sentence can not be in lowercase");
+            }
+
+            var lastSentenceElement = sentence.Value.ToList().Last();
+
+            if (lastSentenceElement is not PunctuationMark)
+            {
+                if (lastSentenceElement is not PunctuationSymbol)
+                {
+                    throw new ArgumentException("last element of sentence should be a punctuation mark or symbol");
+                }
             }
         }
     }
