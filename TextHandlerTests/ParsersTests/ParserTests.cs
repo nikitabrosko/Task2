0bee770950dc6b64640f52061a7352cde04af3ac
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-04 18:08:36 +0300

    Create a debuging test and file for debug

---
+++ TextHandlerTests/ParsersTests/ParserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.Parsers;
+using TextHandler.TextObjectModel;
+
+namespace TextHandlerTests.ParsersTests
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void DebugTextTest()
+        {
+            string path = @"F:\GitHub\Task2\TextHandler\TextHandler\FilesForDebug\file1.txt";
+
+            Parser parser = new Parser();
+
+            Text text = parser.ReadFile(path);
+
+            string str = "1";
+        }
+    }
+}
