b1a421aaf383e0e6d6ba17b2ed70ae5e5e2ed9c1
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-05 23:25:46 +0300

    Update debugging code

---
+++ TextHandlerTests/ToolsTests/TextWorkingToolTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.Parsers;
+using TextHandler.TextObjectModel;
+using TextHandler.Tools;
+
+namespace TextHandlerTests.ToolsTests
+{
+    [TestClass]
+    public class TextWorkingToolTests
+    {
+        [TestMethod]
+        public void DebugTextWorkingToolTest()
+        {
+            string path = @"F:\GitHub\Task2\TextHandler\TextHandler\FilesForDebug\file1.txt";
+
+            Parser parser = new Parser();
+
+            Text text = parser.ReadFile(path);
+
+            var result = TextWorkingTool.RemoveWordsWithGivenLengthThatStartsInConsonantLetter(text, 5);
+
+            string str = "1";
+        }
+    }
+}
