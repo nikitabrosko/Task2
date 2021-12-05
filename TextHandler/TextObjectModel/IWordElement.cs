70220037e0ccfb80760ada162d856d187a2cc731
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-05 14:23:21 +0300

    Create an IWordElement interface

---
+++ TextHandler/TextObjectModel/IWordElement.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHandler.TextObjectModel
+{
+    public interface IWordElement
+    {
+    }
+}
