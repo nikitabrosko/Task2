c934e8877a8b1af4926857ba82ddd6816ebf77c6
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-06 13:41:30 +0300

    Add a FindWordsInQuestionSentences method test class

---
+++ TextHandlerTests/ToolsTests/TextWorkingToolClassTests/WordsInQuestionSentencesMethodTests.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel;
+using TextHandler.TextObjectModel.Characters.Letters;
+using TextHandler.TextObjectModel.Characters.Punctuation;
+using TextHandler.Tools;
+
+namespace TextHandlerTests.ToolsTests.TextWorkingToolClassTests
+{
+    [TestClass]
+    public class WordsInQuestionSentencesMethodTests
+    {
+        [TestMethod]
+        public void TestFindWordsInQuestionSentencesWithValidParameters()
+        {
+            var wordFirst = new Word(new IWordElement[]
+            {
+                new Letter('H'),
+                new Letter('e'),
+                new Letter('l'),
+                new Letter('l'),
+                new Letter('o')
+            });
+            var wordSecond = new Word(new IWordElement[]
+            {
+                new Letter('w'),
+                new Letter('o'),
+                new Letter('r'),
+                new Letter('l'),
+                new Letter('d')
+            });
+
+            var sentenceFirst = new Sentence(new ISentenceElement[]
+            {
+                wordFirst,
+                new PunctuationMark(','),
+                wordSecond,
+                new PunctuationMark('?')
+            });
+            var sentenceSecond = new Sentence(new ISentenceElement[]
+            {
+                new Word(new IWordElement[]
+                {
+                    new Letter('S'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('t'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('c'),
+                    new Letter('e')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('h'),
+                    new Letter('a'),
+                    new Letter('v'),
+                    new Letter('e')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('f'),
+                    new Letter('o'),
+                    new Letter('u'),
+                    new Letter('r')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('w'),
+                    new Letter('o'),
+                    new Letter('r'),
+                    new Letter('d'),
+                    new Letter('s')
+                }),
+                new PunctuationSymbol(new PunctuationMark[]
+                {
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.')
+                })
+            });
+
+            var textObject = new Text();
+            textObject.Append(sentenceFirst);
+            textObject.Append(sentenceSecond);
+
+            IEnumerable<Word> expectedWords = new Word[]
+            {
+                wordFirst,
+                wordSecond
+            };
+            var actualWords = TextWorkingTool.FindWordsInQuestionSentences(textObject, 5);
+
+            Assert.IsTrue(expectedWords.ToArray().SequenceEqual(actualWords.ToArray()));
+        }
+
+        [TestMethod]
+        public void TestFindWordsInQuestionSentencesWithInvalidParametersTextIsNull()
+        {
+            Text textObject = null;
+            var wordLength = 5;
+
+            Assert.ThrowsException<ArgumentNullException>(() =>
+                TextWorkingTool.FindWordsInQuestionSentences(textObject, wordLength), nameof(textObject));
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(-1)]
+        public void TestFindWordsInQuestionSentencesWithInvalidParametersWordLengthIsInvalid(int wordLength)
+        {
+            var sentence = new Sentence(new ISentenceElement[]
+            {
+                new Word(new IWordElement[]
+                {
+                    new Letter('S'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('t'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('c'),
+                    new Letter('e')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('h'),
+                    new Letter('a'),
+                    new Letter('v'),
+                    new Letter('e')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('f'),
+                    new Letter('o'),
+                    new Letter('u'),
+                    new Letter('r')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('w'),
+                    new Letter('o'),
+                    new Letter('r'),
+                    new Letter('d'),
+                    new Letter('s')
+                }),
+                new PunctuationSymbol(new PunctuationMark[]
+                {
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.')
+                })
+            });
+
+            var textObject = new Text();
+            textObject.Append(sentence);
+
+            Assert.ThrowsException<ArgumentException>(
+                () => TextWorkingTool.FindWordsInQuestionSentences(textObject, wordLength), nameof(wordLength));
+        }
+    }
+}
