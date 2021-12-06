7890d124870b8dc8d1b2e0ca7048085162f4a054
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-06 12:02:19 +0300

    Add new ReplaceWordsWithSubstring method and exceptions handlers

--- TextHandler/Tools/TextWorkingTool.cs
+++ TextHandler/Tools/TextWorkingTool.cs
@@ -11,25 +11,34 @@ namespace TextHandler.Tools
     {
         public static IEnumerable<Sentence> SentencesWithOrderByNumberOfWords(Text text)
         {
+            if (text is null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
             return text.Value.OrderByDescending(s => s.Value.Count(e => e is Word));
         }
 
-        public static IEnumerable<Word> FindWordsWithGivenLengthInQuestionSentences(Text text, int length)
+        public static IEnumerable<Word> FindWordsWithGivenLengthInQuestionSentences(Text text, int wordLength)
         {
+            CheckArgumentsForExceptions(text, wordLength);
+
             return text.Value
                 .Where(s => (s.Value.Last() as PunctuationMark)?.Value == '?')
-                .Select(s => s.Value.Where(e => (e as Word)?.Value.Count() == length).Select(w => w as Word).Distinct())
+                .Select(s => s.Value.Where(e => (e as Word)?.Value.Count() == wordLength).Select(w => w as Word).Distinct())
                 .Aggregate((currentSentence, nextSentence) => currentSentence.Union(nextSentence));
         }
 
-        public static Text RemoveWordsWithGivenLengthThatStartsWithConsonantLetter(Text text, int length)
+        public static Text RemoveWordsWithGivenLengthThatStartsWithConsonantLetter(Text text, int wordLength)
         {
+            CheckArgumentsForExceptions(text, wordLength);
+
             var consonants = "bcdfghjklmnpqrstvwxz".ToList();
 
             var sentences = text.Value
                 .Select(s => s.Value.Where(e =>
                     e is PunctuationMark or PunctuationSymbol || 
-                    !(((Word) e).Value.Count() == length && 
+                    !(((Word) e).Value.Count() == wordLength && 
                      consonants.Contains(char.ToLower(((Letter) ((Word) e).Value.First()).Value)))));
 
             Text newText = new Text();
@@ -75,6 +84,70 @@ namespace TextHandler.Tools
 
                     CreateNewText(sentenceElementsList);
                 }
+            }
+        }
+
+        public static Text ReplaceWordsWithSubstring(Text mainText, int sentenceIndex, IEnumerable<ISentenceElement> substringText, int wordLength)
+        {
+            CheckArgumentsForExceptions(mainText, wordLength);
+
+            if (substringText is null)
+            {
+                throw new ArgumentNullException(nameof(substringText));
+            }
+
+            var mainTextTemp = mainText.Value.ToList();
+
+            if (sentenceIndex >= mainTextTemp.Count || sentenceIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sentenceIndex));
+            }
+
+            var previousSentence = mainTextTemp[sentenceIndex];
+
+            var words = previousSentence.Value.Where(e => e is Word)
+                .Where(w => ((Word) w).Value.Count() == wordLength);
+
+            var newSentence = previousSentence.Value.ToList();
+            
+            var newSentenceCount = newSentence.Count;
+
+            for (var i = 0; i < newSentenceCount; i++)
+            {
+                if (words.Contains(newSentence[i]))
+                {
+                    newSentence.RemoveAt(i);
+
+                    foreach (var substringElement in substringText.Reverse())
+                    {
+                        newSentence.Insert(i, substringElement);
+                    }
+                }
+            }
+
+            mainTextTemp.RemoveAt(sentenceIndex);
+            mainTextTemp.Insert(sentenceIndex, new Sentence(newSentence));
+
+            var newText = new Text();
+
+            foreach (var sentence in mainTextTemp)
+            {
+                newText.Append(sentence);
+            }
+
+            return newText;
+        }
+
+        private static void CheckArgumentsForExceptions(Text text, int wordLength)
+        {
+            if (text is null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
+            if (wordLength <= 0)
+            {
+                throw new ArgumentException("word length can not be less or equal 0", nameof(wordLength));
             }
         }
     }
