ebaa4ae4409c961e13ae14aa9f2f2ac3060b0219
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-06 13:41:08 +0300

    Create new folder with tests TextWorkingTool class and add class with SentencesWithOrderByNumberOfWords tests methods

---
+++ TextHandlerTests/ToolsTests/TextWorkingToolClassTests/OrderSentencesMethodTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel;
+using TextHandler.TextObjectModel.Characters.Letters;
+using TextHandler.TextObjectModel.Characters.Punctuation;
+using TextHandler.Tools;
+
+namespace TextHandlerTests.ToolsTests.TextWorkingToolClassTests
+{
+    [TestClass]
+    public class OrderSentencesMethodTests
+    {
+        [TestMethod]
+        public void TestSentencesWithOrderByNumberOfWordsWithValidParameters()
+        {
+            var sentenceFirst = new Sentence(new ISentenceElement[]
+            {
+                new Word(new IWordElement[]
+                {
+                    new Letter('H'),
+                    new Letter('e'),
+                    new Letter('l'),
+                    new Letter('l'),
+                    new Letter('o')
+                }),
+                new PunctuationMark(','),
+                new Word(new IWordElement[]
+                {
+                    new Letter('w'),
+                    new Letter('o'),
+                    new Letter('r'),
+                    new Letter('l'),
+                    new Letter('d')
+                }),
+                new PunctuationMark('!')
+            });
+            var sentenceSecond = new Sentence(new ISentenceElement[]
+            {
+                new Word(new IWordElement[]
+                {
+                    new Letter('S'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('t'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('c'),
+                    new Letter('e')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('h'),
+                    new Letter('a'),
+                    new Letter('v'),
+                    new Letter('e')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('f'),
+                    new Letter('o'),
+                    new Letter('u'),
+                    new Letter('r')
+                }),
+                new Word(new IWordElement[]
+                {
+                    new Letter('w'),
+                    new Letter('o'),
+                    new Letter('r'),
+                    new Letter('d'),
+                    new Letter('s')
+                }),
+                new PunctuationSymbol(new PunctuationMark[]
+                {
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.')
+                })
+            });
+
+            var textObject = new Text();
+            textObject.Append(sentenceFirst);
+            textObject.Append(sentenceSecond);
+
+            var orderingSentences = TextWorkingTool.SentencesWithOrderByNumberOfWords(textObject);
+
+            Assert.IsTrue(textObject.Value.Reverse().ToList().SequenceEqual(orderingSentences.ToList()));
+        }
+
+        [TestMethod]
+        public void TestSentencesWithOrderByNumberOfWordsWithInvalidParameters()
+        {
+            Text text = null;
+
+            Assert.ThrowsException<ArgumentNullException>(() => TextWorkingTool.SentencesWithOrderByNumberOfWords(text),
+                nameof(text));
+        }
+    }
+}
