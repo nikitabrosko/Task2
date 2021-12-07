8031192c15d191f3e5aa73ea9a70274b0e91c504
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-07 21:48:18 +0300

    Update debugging code

---
+++ TextHandlerTests/ParsersTests/ParserFromObjectModelTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.Parsers;
+using TextHandler.TextObjectModel;
+
+namespace TextHandlerTests.ParsersTests
+{
+    [TestClass]
+    public class ParserFromObjectModelTests
+    {
+        [TestMethod]
+        public void TestForDebug()
+        {
+            string pathFrom = @"F:\GitHub\Task2\TextHandler\TextHandler\FilesForDebug\file1.txt";
+            string pathTo = @"F:\GitHub\Task2\TextHandler\TextHandler\FilesForDebug\ParserFromObjectModelFile.txt";
+
+            ParserFromObjectModel parserFromObjectModel = new ParserFromObjectModel();
+            ParserToObjectModel parserToObjectModel = new ParserToObjectModel();
+
+            var textObject = parserToObjectModel.ReadFile(pathFrom);
+            parserFromObjectModel.WriteInFile(pathTo, textObject);
+
+        }
+
+        public void TestForDebug2()
+        {
+            string pathFrom = @"F:\GitHub\Task2\TextHandler\TextHandler\FilesForDebug\file1.txt";
+
+            ParserToObjectModel parserToObjectModel = new ParserToObjectModel();
+
+            var textObject = parserToObjectModel.ReadFile(pathFrom);
+
+            string str = "1";
+        }
+    }
+}
