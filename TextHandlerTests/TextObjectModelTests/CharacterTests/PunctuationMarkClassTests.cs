8d73e403a6ee9b6042f4eae96bc669d97b750947
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-07 13:41:11 +0300

    Create a few new classes with tests Characters classes

---
+++ TextHandlerTests/TextObjectModelTests/CharacterTests/PunctuationMarkClassTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel.Characters.Punctuation;
+
+namespace TextHandlerTests.TextObjectModelTests.CharacterTests
+{
+    [TestClass]
+    public class PunctuationMarkClassTests
+    {
+        [TestMethod]
+        [DataRow('.')]
+        [DataRow(',')]
+        [DataRow('?')]
+        public void TestCreatingWithValidParameters(char punctuationMark)
+        {
+            var punctuationMarkObject = new PunctuationMark(punctuationMark);
+
+            Assert.AreEqual(punctuationMark, punctuationMarkObject.Value);
+        }
+
+        [TestMethod]
+        [DataRow('a')]
+        [DataRow('1')]
+        [DataRow(' ')]
+        public void TestCreatingWithInvalidParameters(char punctuationMark)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new PunctuationMark(punctuationMark),
+                nameof(punctuationMark));
+        }
+    }
+}
---
+++ TextHandlerTests/TextObjectModelTests/CharacterTests/PunctuationSymbolClassTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel.Characters.Punctuation;
+
+namespace TextHandlerTests.TextObjectModelTests.CharacterTests
+{
+    [TestClass]
+    public class PunctuationSymbolClassTests
+    {
+        [TestMethod]
+        public void TestCreatingWithValidParametersLengthThree()
+        {
+            var punctuationMarks = new List<PunctuationMark>
+            {
+                new PunctuationMark('.'),
+                new PunctuationMark('.'),
+                new PunctuationMark('.')
+            };
+
+            var punctuationSymbolObject = new PunctuationSymbol(punctuationMarks);
+
+            Assert.IsTrue(punctuationMarks.SequenceEqual(punctuationSymbolObject.Value.ToList()));
+        }
+
+        [TestMethod]
+        public void TestCreatingWithValidParametersLengthTwo()
+        {
+            var punctuationMarks = new List<PunctuationMark>
+            {
+                new PunctuationMark('?'),
+                new PunctuationMark('!')
+            };
+
+            var punctuationSymbolObject = new PunctuationSymbol(punctuationMarks);
+
+            Assert.IsTrue(punctuationMarks.SequenceEqual(punctuationSymbolObject.Value.ToList()));
+        }
+
+        [TestMethod]
+        public void TestCreatingWithInvalidParametersPunctuationMarksLengthThreeIsWrong()
+        {
+            var punctuationMarks = new List<PunctuationMark>
+            {
+                new PunctuationMark(','),
+                new PunctuationMark('.'),
+                new PunctuationMark('!')
+            };
+
+            Assert.ThrowsException<ArgumentException>(() => new PunctuationSymbol(punctuationMarks),
+                nameof(PunctuationSymbol));
+        }
+
+        [TestMethod]
+        public void TestCreatingWithInvalidParametersPunctuationMarksLengthTwoIsWrong()
+        {
+            var punctuationMarks = new List<PunctuationMark>
+            {
+                new PunctuationMark('.'),
+                new PunctuationMark('.')
+            };
+
+            Assert.ThrowsException<ArgumentException>(() => new PunctuationSymbol(punctuationMarks),
+                nameof(PunctuationSymbol));
+        }
+
+        [TestMethod]
+        public void TestCreatingWithInvalidParametersPunctuationMarksIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new PunctuationSymbol(null),
+                nameof(PunctuationSymbol.Value));
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(4)]
+        public void TestCreatingWithInvalidParametersPunctuationSymbolLengthIsInvalid(int length)
+        {
+            var punctuationMarks = new List<PunctuationMark>(length);
+
+            for (int i = 0; i < length; i++)
+            {
+                punctuationMarks.Add(new PunctuationMark('.'));
+            }
+
+            Assert.ThrowsException<ArgumentException>(() => new PunctuationSymbol(punctuationMarks),
+                nameof(PunctuationSymbol));
+        }
+    }
+}
