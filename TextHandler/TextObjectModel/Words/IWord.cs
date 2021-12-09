da290b7b781d6c52756546f57da8b56089bf2e1a
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-10 00:21:51 +0300

    Create folder Words, class Word and interface IWord

---
+++ TextHandler/TextObjectModel/Words/IWord.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace TextHandler.TextObjectModel.Words
+{
+    public interface IWord : ISentenceElement, ITextModelElement<IEnumerable<IWordElement>>
+    {
+    }
+}
---
+++ TextHandler/TextObjectModel/Words/Word.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using TextHandler.TextObjectModel.SpellingMarks;
+
+namespace TextHandler.TextObjectModel.Words
+{
+    public class Word : IWord
+    {
+        private readonly IList<IWordElement> _word;
+
+        public IEnumerable<IWordElement> Value => new ReadOnlyCollection<IWordElement>(_word);
+
+        public Word(IEnumerable<IWordElement> wordElements)
+        {
+            _word = wordElements.ToList();
+
+            try
+            {
+                Verify();
+            }
+            catch
+            {
+                _word = default;
+
+                throw;
+            }
+        }
+
+        private void Verify()
+        {
+            var wordList = Value.ToList();
+
+            if (wordList.Count == 0)
+            {
+                throw new ArgumentException("letters count can not be 0");
+            }
+
+            if (Value.First() is ISpellingMark)
+            {
+                throw new ArgumentException("first element of word can not be a punctuation mark");
+            }
+        }
+    }
+}
