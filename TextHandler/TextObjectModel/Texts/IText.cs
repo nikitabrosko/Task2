88674dc5656640e716a2bfd864ca0cd6408857a7
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-10 00:21:27 +0300

    Create folder Texts, class Text and interface IText

---
+++ TextHandler/TextObjectModel/Texts/IText.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using TextHandler.TextObjectModel.Sentences;
+
+namespace TextHandler.TextObjectModel.Texts
+{
+    public interface IText : ITextModelElement<IEnumerable<ISentence>>
+    {
+        void Append(ISentence sentence);
+    }
+}
---
+++ TextHandler/TextObjectModel/Texts/Text.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using TextHandler.TextObjectModel.Sentences;
+
+namespace TextHandler.TextObjectModel.Texts
+{
+    public class Text : IText
+    {
+        private readonly IList<ISentence> _text = new List<ISentence>();
+
+        public IEnumerable<ISentence> Value => new ReadOnlyCollection<ISentence>(_text);
+
+        public void Append(ISentence sentence)
+        {
+            _text.Add(sentence);
+        }
+    }
+}
