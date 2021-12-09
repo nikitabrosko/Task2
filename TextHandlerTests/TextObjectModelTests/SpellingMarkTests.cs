bf8049cff2da6cc65b01515dcf59072ff765f438
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-10 01:14:54 +0300

    Create new SpellingMarkTests test class

---
+++ TextHandlerTests/TextObjectModelTests/SpellingMarkTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel.SpellingMarks;
+
+namespace TextHandlerTests.TextObjectModelTests
+{
+    [TestClass]
+    public class SpellingMarkTests
+    {
+        [TestMethod]
+        [DataRow('-')]
+        [DataRow('\'')]
+        public void TestCreatingWithValidParameters(char spellingMark)
+        {
+            var spellingMarkObject = new SpellingMark(spellingMark);
+
+            Assert.AreEqual(spellingMark, spellingMarkObject.Value);
+        }
+
+        [TestMethod]
+        [DataRow('1')]
+        [DataRow('!')]
+        public void TestCreatingWithInvalidParameters(char spellingMark)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new SpellingMark(spellingMark));
+        }
+    }
+}
