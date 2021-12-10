ca491435e83cc6e1c7465692ebd1c35e7bd420d0
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-10 18:40:46 +0300

    Add new WhiteSpaceTests tests class

---
+++ TextHandlerTests/TextObjectModelTests/WhiteSpaceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel.WhiteSpaces;
+
+namespace TextHandlerTests.TextObjectModelTests
+{
+    [TestClass]
+    public class WhiteSpaceTests
+    {
+        [TestMethod]
+        public void TestCreatingWithValidParameters()
+        {
+            var character = ' ';
+
+            var whiteSpaceObject = new WhiteSpace(character);
+
+            Assert.AreEqual(whiteSpaceObject.Value, character);
+        }
+
+        [TestMethod]
+        [DataRow('.')]
+        [DataRow('1')]
+        [DataRow('a')]
+        [DataRow('\t')]
+        public void TestCreatingWithInvalidParameters(char character)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new WhiteSpace(character));
+        }
+    }
+}
