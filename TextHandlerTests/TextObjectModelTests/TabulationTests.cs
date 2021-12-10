6bd228cc3c2ea61c8ce4247e8ff8294e1751293c
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-10 18:41:34 +0300

    Add new TabulationTests tests class

---
+++ TextHandlerTests/TextObjectModelTests/TabulationTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextHandler.TextObjectModel.Tabulations;
+
+namespace TextHandlerTests.TextObjectModelTests
+{
+    [TestClass]
+    public class TabulationTests
+    {
+        [TestMethod]
+        public void TestCreatingParameterIsValid()
+        {
+            var character = '\t';
+
+            var tabulationObject = new Tabulation(character);
+
+            Assert.AreEqual(tabulationObject.Value, character);
+        }
+
+        [TestMethod]
+        [DataRow('.')]
+        [DataRow('a')]
+        [DataRow('1')]
+        [DataRow('\n')]
+        public void TestCreatingParameterIsInvalid(char character)
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Tabulation(character));
+        }
+    }
+}
