af621790578a19c0c0c3eea02bd83161c5b7d6ae
Nik.Brosko <Nikita.brosko@mail.ru>
2021-12-10 18:41:11 +0300

    Add new test method to TextTests tests class

--- TextHandlerTests/TextObjectModelTests/TextTests.cs
+++ TextHandlerTests/TextObjectModelTests/TextTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TextHandler.TextObjectModel;
 using TextHandler.TextObjectModel.Letters;
+using TextHandler.TextObjectModel.NewLines;
 using TextHandler.TextObjectModel.Punctuations.PunctuationMarks;
 using TextHandler.TextObjectModel.Punctuations.PunctuationSymbols;
 using TextHandler.TextObjectModel.Sentences;
@@ -93,6 +94,63 @@ namespace TextHandlerTests.TextObjectModelTests
                               .Equals((textObject.Value.First() as ISentence)?.GetStringRepresentation()) 
                           && sentenceSecond.GetStringRepresentation()
                               .Equals((textObject.Value.Last() as ISentence)?.GetStringRepresentation()));
+        }
+
+        [TestMethod]
+        public void TestTextClassCreatingWithNewLine()
+        {
+            var sentenceFirst = new Sentence(new ISentenceElement[]
+            {
+                new Word(new IWordElement[]
+                {
+                    new Letter('H'),
+                    new Letter('e'),
+                    new Letter('l'),
+                    new Letter('l'),
+                    new Letter('o')
+                }),
+                new PunctuationMark(','),
+                new WhiteSpace(' '),
+                new Word(new IWordElement[]
+                {
+                    new Letter('w'),
+                    new Letter('o'),
+                    new Letter('r'),
+                    new Letter('l'),
+                    new Letter('d')
+                }),
+                new PunctuationMark('!')
+            });
+            var newLine = new NewLine('\n');
+            var sentenceSecond = new Sentence(new ISentenceElement[]
+            {
+                new Word(new IWordElement[]
+                {
+                    new Letter('S'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('t'),
+                    new Letter('e'),
+                    new Letter('n'),
+                    new Letter('c'),
+                    new Letter('e')
+                }),
+                new PunctuationSymbol(new IPunctuationMark[]
+                {
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.'),
+                    new PunctuationMark('.')
+                })
+            });
+
+            var textObject = new Text();
+            textObject.Append(sentenceFirst);
+            textObject.Append(newLine);
+            textObject.Append(sentenceSecond);
+
+            Assert.AreEqual(
+                string.Concat(sentenceFirst.GetStringRepresentation(), newLine.GetStringRepresentation(),
+                    sentenceSecond.GetStringRepresentation()), textObject.GetStringRepresentation());
         }
     }
 }
